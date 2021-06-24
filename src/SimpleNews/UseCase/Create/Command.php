543a1222ac1ae97032ac25894034f90acb642769
Иван Лещев <punkundead@gmail.com>
2021-06-24 17:41:43 +0300

    simplenews create and update validation

--- src/SimpleNews/UseCase/Create/Command.php
+++ src/SimpleNews/UseCase/Create/Command.php
@@ -4,10 +4,14 @@ declare(strict_types=1);
 
 namespace App\SimpleNews\UseCase\Create;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
     public function __construct(
+        #[Assert\Length(max: 255)]
         public string $title,
+        #[Assert\Length(max: 255)]
         public string $author,
         public string $body,
         public ?string $date = null
--- src/SimpleNews/UseCase/Update/Command.php
+++ src/SimpleNews/UseCase/Update/Command.php
@@ -4,11 +4,15 @@ declare(strict_types=1);
 
 namespace App\SimpleNews\UseCase\Update;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 class Command
 {
     public function __construct(
         public int $id,
+        #[Assert\Length(max: 255)]
         public ?string $title,
+        #[Assert\Length(max: 255)]
         public ?string $author,
         public ?string $body
     ) {
